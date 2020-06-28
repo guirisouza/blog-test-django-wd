5170b8b2b5a81e781ba13ae99d203d84a9e5e9ee
Guilherme <guilherme@pop-os.localdomain>
2020-06-28 02:16:51 -0300

    create models Post and Category

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    slug = models.CharField(max_length=60)
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    slug = models.CharField(max_lenght=60)
+    title = models.CharField(max_lenght=60)
